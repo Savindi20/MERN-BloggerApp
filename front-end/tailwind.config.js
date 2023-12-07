f2f1ccb7b20c4f8d95fafffd0f70ac89b397b7c8
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-07 13:06:25 +0530

    project refactored

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1565D8",
+        dark: {
+          soft: "#183B56",
+          hard: "#0D2436",
+          light: "#5A7184"
+        },
+      },
+      fontFamily: {
+        Ubuntu: ["Ubuntu", "sans-serif"],
+        babylonica: ["Babylonica", "cursive"],
+        opensans:["Open Sans", "sans-serif"],
+        roboto:["Roboto", "sans-serif"],
+        firasans: ["Fira Sans", "sans-serif"],
+        heading: ["Play", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
