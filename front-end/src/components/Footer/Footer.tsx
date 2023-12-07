4dbd95b4b830e5f39e4a9bec935427e7a44d9b71
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-07 19:43:15 +0530

    footer basic navigation part added

--- front-end/src/components/Footer/Footer.tsx
+++ front-end/src/components/Footer/Footer.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
 
-function Footer() {
+const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="bg-dark-hard">
+      {/* start footer section  */}
+      <footer className="container mx-auto grid grid-cols-10 px-5 py-10 gap-y-10 gap-x-5 md:pt-20 md:grid-cols-12 lg:grid-cols-10 lg:gap-x-10">
+        {/* start navigations */}
+        <div className="col-span-5 md:col-span-4 lg:col-span-2">
+          <h3 className="text-dark-light font-bold md:text-lg">Product</h3>
+          <ul className="text-[#959EAD] text-sm mt-5 space-y-4 md:text-base">
+            <li>
+              <a href="/">Landingpage</a>
+            </li>
+            <li>
+              <a href="/">Features</a>
+            </li>
+            <li>
+              <a href="/">Pricing</a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+      {/* end footer section  */}
+    </section>
+  );
+};
 
-export default Footer
+export default Footer;
