ea6c92b1eb1a2ca4b24efc0ca3913d57fb2a0691
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-08 01:21:22 +0530

    header bbasic section added

--- front-end/src/components/Header/Header.tsx
+++ front-end/src/components/Header/Header.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 function Header() {
+
   return (
-    <div>
-      
-    </div>
+    <section className="sticky top-0 left-0 right-0 z-50 bg-white">
+      <header className="container mx-auto px-5 flex justify-between py-4 items-center">
+        <div>{/* <img className="w-16" src="" alt="logo" /> */}</div>
+        <div className="lg:hidden z-50">
+        </div>
+      </header>
+    </section>
   )
 }
 
