6ac7c222d57571175dec8a62337d83aa5d690763
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-14 13:12:15 +0530

    show artical and more artical details added

--- front-end/src/components/Articles/Articles.tsx
+++ front-end/src/components/Articles/Articles.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
-function Articles() {
+const Articles = () => {
+  const [showAllPosts, setShowAllPosts] = useState(false);
+
+  const handleShowAllPosts = () => {
+    setShowAllPosts(true);
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="flex flex-col container mx-auto px-5 py-10">
+      {!showAllPosts && (
+        <button
+          onClick={handleShowAllPosts}
+          className="mx-auto flex items-center gap-x-2 font-bold text-purple-600 border-2 border-purple-600 px-6 py-3 rounded-lg transition duration-300 ease-in-out hover:bg-gradient-to-r hover:from-purple-600 hover:to-purple-800 hover:border-purple-800 hover:text-white"
+        >
+          <span>More articles</span>
+          <KeyboardArrowRightIcon className="w-3 h-3" />
+        </button>
+      )}
+    </section>
+  );
+};
 
-export default Articles
+export default Articles;
