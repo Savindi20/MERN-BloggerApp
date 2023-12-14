4f15e3d0f592e4be2966dd7f4a58f83fa3427207
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-14 11:45:30 +0530

    header and footer added to the main layout

--- front-end/src/components/MainLayout/MainLayout.tsx
+++ front-end/src/components/MainLayout/MainLayout.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { ReactNode } from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
-function MainLayout() {
+interface MainLayoutProps {
+  children: ReactNode;
+}
+const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <div>
-      
+      <Header />
+      {children}
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
