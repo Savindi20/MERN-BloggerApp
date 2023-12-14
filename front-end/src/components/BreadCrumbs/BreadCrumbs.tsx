1b0eaf25d49de6aae90682faea5339f712c12d3c
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-15 02:51:30 +0530

    breadCrumb file updated

--- front-end/src/components/BreadCrumbs/BreadCrumbs.tsx
+++ front-end/src/components/BreadCrumbs/BreadCrumbs.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-function BreadCrumbs() {
-  return (
-    <div>BreadCrumbs</div>
-  )
+interface BreadCrumbsProps {
+  data: {
+    name: string;
+    link: string;
+  }[];
 }
+// this code added navigation paths in the page top
+const BreadCrumbs: React.FC<BreadCrumbsProps> = ({ data }) => {
+  return (
+    <div className="flex items-center py-4 overflow-x-auto whitespace-nowrap">
+      {data.map((item, index) => (
+        <div
+          key={index}
+          className="text-black opacity-50 text-xs font-Ubuntu md:text-sm"
+        >
+          <Link to={item.link}>{item.name}</Link>
+          {index !== data.length - 1 && <span className="px-3">/</span>}
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default BreadCrumbs
+export default BreadCrumbs;
