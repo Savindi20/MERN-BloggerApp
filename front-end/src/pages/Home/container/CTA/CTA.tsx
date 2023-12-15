ac46ca54ef1ad4765cd1422eb5045c74ead1c372
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-15 11:33:57 +0530

    CTA container updated with svg

--- front-end/src/pages/Home/container/CTA/CTA.tsx
+++ front-end/src/pages/Home/container/CTA/CTA.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
-function CTA() {
+const CTA = () => {
   return (
-    <div>CTA</div>
-  )
-}
+    <>
+      <svg
+        className="w-full h-auto max-h-40 translate-y-[1px]"
+        preserveAspectRatio="none"
+        viewBox="0 0 2160 263"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          id="Wave"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M2160 262.5H0V0C360 80 720 120 1080 120C1440 120 1800 80 2160 0V262.5Z"
+          fill="#0D2436"
+        />
+      </svg>
+    </>
+  );
+};
 
-export default CTA
+export default CTA;
