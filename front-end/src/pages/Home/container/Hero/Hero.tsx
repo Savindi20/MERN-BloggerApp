1fd8a086aeef6300cdda2fb42650fef2cb08bc3c
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-15 16:02:59 +0530

    Hero container main section added

--- front-end/src/pages/Home/container/Hero/Hero.tsx
+++ front-end/src/pages/Home/container/Hero/Hero.tsx
@@ -9,6 +9,38 @@ const Hero = () => {
         <h1 className="font-ubuntu text-3xl text-center font-bold text-dark-soft md:text-5xl lg:text-4xl xl:text-5xl lg:text-left lg:max-w-[540px]">
           Read the most interesting articles
         </h1>
+        <p className="text-dark-light mt-4 text-center md:text-xl lg:text-base xl:text-xl lg:text-left">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga mollitia
+          dolorem beatae ipsa, laboriosam assumenda perspiciatis aut culpa ex,
+          numquam deleniti distinctio! Eos, doloremque. Laboriosam repudiandae
+          cumque amet totam minus?
+        </p>
+        <div className="flex flex-col gap-y-2.5 mt-10 lg:mt-6 xl:mt-10 relative">
+          <div className="relative">
+            <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 w-6 h-6 text-[#959EAD]" />
+            <input
+              className="placeholder:font-bold placeholder:font-ubuntu font-semibold text-dark-soft placeholder:text-[#959EAD] rounded-lg pl-12 pr-3 w-full py-3 focus:outline-none shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px]  md:py-4"
+              type="text"
+              placeholder="Search article"
+            />
+          </div>
+          <button className="w-full bg-purple-600 text-white font-semibold rounded-lg px-5 py-3 md:absolute md:right-2 md:top-1/2 md:-translate-y-1/2 md:w-fit md:py-2">
+            Search
+          </button>
+        </div>
+        <div className="flex mt-4 flex-col lg:flex-row lg:items-start lg:flex-nowrap lg:gap-x-4 lg:mt-7">
+          <span className="text-dark-light font-semibold italic mt-2 lg:mt-4 lg:text-sm xl:text-base">
+            Popular Tags
+          </span>
+          <ul className="flex flex-wrap gap-x-2.5 gap-y-2.5 mt-3 lg:text-sm xl:text-base">
+            <li className="rounded-lg bg-purple-600 bg-opacity-10 px-3 py-1.5 text-purple-600 font-semibold">
+              UI
+            </li>
+            <li className="rounded-lg bg-purple-600 bg-opacity-10 px-3 py-1.5 text-purple-600 font-semibold">
+              UX
+            </li>
+          </ul>
+        </div>
       </div>
 
       <div className="hidden lg:block lg:1/2">
