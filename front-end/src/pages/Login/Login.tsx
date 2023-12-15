43e3f956a06b4cc2968c5d9c0d45a14fb416b085
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-15 19:58:56 +0530

    login basic interface added

--- front-end/src/pages/Login/Login.tsx
+++ front-end/src/pages/Login/Login.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import MainLayout from "../../components/MainLayout";
+import { Link } from "react-router-dom";
 
-function Login() {
+const Login = () => {
+
   return (
-    <div>Login</div>
-  )
+    <MainLayout>
+      <section className="container mx-auto px-5 py-10 font-opensans">
+        <div className="w-full max-w-sm mx-auto">
+          <h1 className="font-roboto text-2xl font-bold text-center text-dark-hard mb-8">
+            Login
+          </h1>
+          <form>
+            <Link to="" className="text-sm font-semibold text-primary">
+              Forgot password?
+            </Link>
+            <button
+              type="submit"
+              disabled={false}
+              className="bg-purple-600 text-white font-bold text-lg py-4 px-8 w-full rounded-lg my-6 disabled:opacity-70 disabled:cursor-not-allowed"
+            >
+              Sign In
+            </button>
+            <p className="text-sm font-semibold text-[#5a7184]">
+              Do not have an account?{" "}
+              <Link to="/register" className="text-primary">
+                Register now
+              </Link>
+            </p>
+          </form>
+        </div>
+      </section>
+    </MainLayout>
+  );
 }
 
-export default Login
+export default Login;
