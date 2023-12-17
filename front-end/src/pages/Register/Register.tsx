ca86ba405198dee42e3cd80c87c2a47b55da1375
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-17 16:45:22 +0530

    register form data added

--- front-end/src/pages/Register/Register.tsx
+++ front-end/src/pages/Register/Register.tsx
@@ -1,5 +1,6 @@
-import { ChangeEvent } from "react";
+import React from "react";
 import MainLayout from "../../components/MainLayout";
+import { Link } from "react-router-dom";
 
 function Register() {
 
@@ -10,6 +11,54 @@ function Register() {
           <h1 className="font-roboto text-2xl font-bold text-center text-dark-hard mb-8">
             Sign Up
           </h1>
+          <form>
+            <div className="flex flex-col mb-6 w-full">
+              <label
+                htmlFor="name"
+                className="text-[#5a7184] font-semibold block"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Enter name"
+                pattern="[A-Za-z\s]+"
+                required
+                className="placeholder:text-[#959ead] text-dark-hard mt-3 rounded-lg px-5 py-4 font-semibold block outline-none border"
+              />
+            </div>
+            <div className="flex flex-col mb-6 w-full">
+              <label
+                htmlFor="email"
+                className="text-[#5a7184] font-semibold block"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Enter email"
+                pattern="^(([^<>()[\]\\.,;:\s@]+(\.[^<>()[\]\\.,;:\s@]+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$"
+                required
+                className="placeholder:text-[#959ead] text-dark-hard mt-3 rounded-lg px-5 py-4 font-semibold block outline-none border"
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-purple-600 text-white font-bold text-lg py-4 px-8 w-full rounded-lg mb-6 disabled:opacity-70 disabled:cursor-not-allowed"
+            >
+              Register
+            </button>
+            <p className="text-sm font-semibold text-[#5a7184]">
+              You have an account?{" "}
+              <Link to="/login" className="text-primary">
+                Login now
+              </Link>
+            </p>
+          </form>
         </div>
       </section>
     </MainLayout>
