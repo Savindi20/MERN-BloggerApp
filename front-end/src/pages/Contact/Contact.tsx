2d168c2ec64a2a8b6a34ca1078ae342d8feedaa4
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-18 00:07:21 +0530

    contact file updated with form details

--- front-end/src/pages/Contact/Contact.tsx
+++ front-end/src/pages/Contact/Contact.tsx
@@ -12,6 +12,54 @@ function Contact() {
             </h1>
           </div>
           <div className="flex flex-col md:flex-row mt-8 gap-x-12 px-16">
+
+            <form className="w-full md:w-1/2">
+              <h1 className="text-2xl lg:text-3xl font-bold mb-4 text-gray-400">
+                Get in Touch
+              </h1>
+              <div className="mb-4">
+                <input
+                  type="text"
+                  id="name"
+                  placeholder="Your Name"
+                  className="w-full bg-gray-200 appearance-none border-2 border-gray-200 rounded-2xl py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 shadow-sm"
+                />
+              </div>
+              <div className="mb-4">
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="Your Email"
+                  className="w-full bg-gray-200 appearance-none border-2 border-gray-200 rounded-2xl py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 shadow-sm"
+                />
+              </div>
+              <div className="mb-4">
+                <input
+                  type="text"
+                  id="subject"
+                  placeholder="Subject"
+                  className="w-full bg-gray-200 appearance-none border-2 border-gray-200 rounded-2xl py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 shadow-sm"
+                />
+              </div>
+              <div className="mb-4">
+                <textarea
+                  id="message"
+                  placeholder="Your Message"
+                  className="w-full bg-gray-200 appearance-none border-2 border-gray-200 rounded-2xl py-2 px-4 mb-2 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 shadow-sm"
+                  cols={30}
+                  rows={5}
+                ></textarea>
+              </div>
+              <div className="flex justify-center">
+                <button
+                  type="submit"
+                  disabled={false}
+                  className="btn bg-purple-500 text-white px-6 py-2 rounded-full hover:bg-purple-600 w-full shadow-sm"
+                >
+                  Send Message
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </section>
