11d39501dbcf23ba785e87e23d6541c19ccd145c
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-17 15:42:13 +0530

    register form basic sign up div added

--- front-end/src/pages/Register/Register.tsx
+++ front-end/src/pages/Register/Register.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { ChangeEvent } from "react";
+import MainLayout from "../../components/MainLayout";
 
 function Register() {
+
   return (
-    <div>Register</div>
-  )
+    <MainLayout>
+      <section className="container mx-auto px-5 py-10 font-opensans">
+        <div className="w-full max-w-sm mx-auto">
+          <h1 className="font-roboto text-2xl font-bold text-center text-dark-hard mb-8">
+            Sign Up
+          </h1>
+        </div>
+      </section>
+    </MainLayout>
+  );
 }
 
-export default Register
+export default Register;
