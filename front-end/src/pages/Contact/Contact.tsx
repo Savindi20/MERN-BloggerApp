5bda2d5222f738bbae9a53cb9088d7e7cbdfa596
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-17 23:49:31 +0530

    Home page contact tsx file updated

--- front-end/src/pages/Contact/Contact.tsx
+++ front-end/src/pages/Contact/Contact.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import MainLayout from "../../components/MainLayout/MainLayout";
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <MainLayout>
+      <section className="container mx-auto px-5 py-10">
+        <div className="container mx-auto px-4">
+          <div className="heading text-center">
+            <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">
+              I'm Very Responsive to Messages
+            </h1>
+          </div>
+          <div className="flex flex-col md:flex-row mt-8 gap-x-12 px-16">
+          </div>
+        </div>
+      </section>
+    </MainLayout>
+  );
 }
 
-export default Contact
+export default Contact;
