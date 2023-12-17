aa1ac5bec6789006ed18774786b8f2f5e3ada5eb
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-18 04:42:15 +0530

    comment component comment form updated

--- front-end/src/components/Comments/CommentForm/CommentForm.tsx
+++ front-end/src/components/Comments/CommentForm/CommentForm.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function CommentForm() {
-  return (
-    <div>CommentForm</div>
-  )
+interface CommentsFormProps {
+  btnLabel?: string;
+  formSubmitHandler: (comment: string) => void;
+  formCancleHandler: any;
+  initialText:any;
 }
 
-export default CommentForm
+const CommentForm = ({
+  btnLabel,
+  formSubmitHandler,
+  formCancleHandler = null,
+  initialText="",
+}: CommentsFormProps) => {
+  // start value state
+  const [value, setvalue] = useState(initialText);
+  // end value state
+
+  // start submitHandler
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    formSubmitHandler(value);
+    setvalue("");
+  };
+  // end submitHandler
+
+  return (
+    // start form
+    <form onSubmit={submitHandler}>
+      <div className="flex flex-col items-end border border-purple-600 rounded-lg p-4">
+         {/* start textarea  */}
+        <textarea
+          className="w-full focus:outline-none bg-transparent"
+          rows={5}
+          placeholder="Leave your comment here..."
+          value={value}
+          onChange={(e) => setvalue(e.target.value)}
+        />
+        {/* end textarea & start button */}
+        <div className="flex flex-col-reverse gap-y-2 items-center gap-x-2 pt-2 min-[420px]:flex-row">
+          {formCancleHandler && <button onClick={formCancleHandler} className="px-6 py-2.5 rounded-lg border border-red-500 text-red-500">
+            Cancel</button>}
+          <button
+            type="submit"
+            className="px-6 py-2.5 rounded-lg bg-purple-600 text-white font-semibold"
+          >
+            {btnLabel}
+          </button>
+        </div>
+        {/* end button */}
+      </div>
+    </form>
+    // end form
+  );
+};
+
+export default CommentForm;
