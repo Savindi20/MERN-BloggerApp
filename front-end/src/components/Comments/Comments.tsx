2c44e0254aa8a5453816618098a044e3c83dfc3a
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-18 05:06:23 +0530

    comment component  updated

--- front-end/src/components/Comments/Comments.tsx
+++ front-end/src/components/Comments/Comments.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import images from "../../constants/Images/images";
 
-function Comments() {
-  return (
-    <div>Comments</div>
-  )
+interface CommentsProps {
+  comment: any;
+  logginedUserId: string;
+  affectedComment: any;
+  setAffectedComment: any;
+  addComment: any;
+  parentId: any;
+  updateComment: any;
+  deleteComment: any;
+  replies: any;
 }
 
-export default Comments
+const Comments = ({
+}: CommentsProps) => {
+
+  return (
+    // start comment wrapper
+    <div className="flex flex-nowrap items-start gap-x-3 bg-[#F2F4F5] p-3 rounded-lg">
+      {/* start user profile picture */}
+      <img
+        src={images.PostProfileImage}
+        alt="user profile"
+        className="w-9 h-9 object-cover rounded-full"
+      />
+    </div>
+    // end comment wrapper
+  );
+};
+
+export default Comments;
