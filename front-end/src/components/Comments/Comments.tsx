31d8f17b9f1ea68c0c01c320ce47c9de84c59970
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-18 05:54:12 +0530

    comment component updated with profile details

--- front-end/src/components/Comments/Comments.tsx
+++ front-end/src/components/Comments/Comments.tsx
@@ -14,6 +14,7 @@ interface CommentsProps {
 }
 
 const Comments = ({
+  comment
 }: CommentsProps) => {
 
   return (
@@ -25,6 +26,23 @@ const Comments = ({
         alt="user profile"
         className="w-9 h-9 object-cover rounded-full"
       />
+      {/* end user profile picture */}
+      <div className="flex-1 flex flex-col">
+        {/* start user name */}
+        <h5 className="font-bold text-dark-hard text-xs lg:text-sm">
+          {comment.user.name}
+        </h5>
+        {/* end user name */}
+        {/* start commented date */}
+        <span className="text-ts text-dark-light">
+          {new Date(comment.createdAt).toLocaleDateString("en-US", {
+            day: "numeric",
+            month: "short",
+            year: "numeric",
+            hour: "2-digit",
+          })}
+        </span>
+      </div>
     </div>
     // end comment wrapper
   );
