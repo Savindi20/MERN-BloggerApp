ac8b7d155fa2ff67aa16f5dbd6a4485e4f2f44f1
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-19 22:34:38 +0530

    Social share button component updated and completed

--- front-end/src/components/SocialShareButtons/SocialShareButtons.tsx
+++ front-end/src/components/SocialShareButtons/SocialShareButtons.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import TwitterIcon from "@mui/icons-material/Twitter";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
 
 interface SocialShareButtonsProps{
     url:any;
@@ -8,6 +12,34 @@ interface SocialShareButtonsProps{
 const SocialShareButtons = ({url, title }:SocialShareButtonsProps) => {
   return (
     <div className="flex gap-4">
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href={`=${url}`}
+      >
+        <FacebookIcon className="text-[#3b5998] w-12 h-auto transition-transform duration-300 hover:scale-105 shadow-2xl" />
+      </a>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href={`=${url}`}
+      >
+        <InstagramIcon className="text-[#E1306C] w-12 h-auto transition-transform duration-300 hover:scale-105 shadow-2xl" />
+      </a>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href={`=${url}`}
+      >
+        <GitHubIcon className="text-[#171515] w-12 h-auto transition-transform duration-300 hover:scale-105 shadow-2xl" />
+      </a>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href={`=${url}`}
+      >
+        <TwitterIcon className="text-[#1DA1F2] w-12 h-auto transition-transform duration-300 hover:scale-105 shadow-2xl" />
+      </a>
     </div>
   )
 }
