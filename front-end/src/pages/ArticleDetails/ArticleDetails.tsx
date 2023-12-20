bab475da761138ec8916b1ab1f101b2ff2a85f34
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-20 21:02:21 +0530

    sudgest data part and tempory data added

--- front-end/src/pages/ArticleDetails/ArticleDetails.tsx
+++ front-end/src/pages/ArticleDetails/ArticleDetails.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import MainLayout from "../../components/MainLayout/MainLayout";
 import BreadCrumbs from "../../components/BreadCrumbs/BreadCrumbs";
+import images from "../../constants/Images/images";
 import { Link} from "react-router-dom";
+import SuggestPosts from "./container/SuggestedPosts/SuggestPosts";
 import CommentsContainer from "../../components/Comments/CommentsContainer/CommentsContainer";
 import SocialShareButtons from "../../components/SocialShareButtons/SocialShareButtons";
 import axios from "../../axios";
 import { PostDetails } from "../../types/PostDetails";
-import { image } from "../../types/Image";
 
 const breadCrumbsData = [
   { name: "Home", link: "/" },
@@ -14,11 +15,49 @@ const breadCrumbsData = [
   { name: "Article title", link: `/blog/1` },
 ];
 
+// start added tempory data to the posts
+const postsData = [
+  {
+    _id: "1",
+    image: images.Post1Image,
+    title: "Help ui ux engineer get better education",
+    createdAt: "2023-11-28T15:35:53.607+0000",
+  },
+  {
+    _id: "2",
+    image: images.Post1Image,
+    title: "Help ui ux engineer get better education",
+    createdAt: "2023-11-28T15:35:53.607+0000",
+  },
+  {
+    _id: "3",
+    image: images.Post1Image,
+    title: "Help ui ux engineer get better education",
+    createdAt: "2023-11-28T15:35:53.607+0000",
+  },
+  {
+    _id: "4",
+    image: images.Post1Image,
+    title: "Help ui ux engineer get better education",
+    createdAt: "2023-11-28T15:35:53.607+0000",
+  },
+];
+// end added tempory data to the posts
+
+// start tempory tag data
+const tagsData: string[] = [
+  "UI UX",
+  "Theams",
+  "React",
+  "React Native",
+  "Tailwind CSS",
+  "Education",
+];
+// end tempory tag data
+
 const ArticleDetails = (): JSX.Element => {
   const [postList, setPostList] = useState<PostDetails[]>([]);
   const [post, setPost] = useState<PostDetails | null>(null); // Use PostDetails type for post state
-  const [imageList, setImageList] = useState<image[]>([]); // imageList: image[]
-  const [allPosts, setAllPosts] = useState<PostDetails[]>([]); // Use PostDetails type for allPosts state
 
   useEffect(() => {
     // use effect
@@ -49,9 +88,7 @@ const ArticleDetails = (): JSX.Element => {
             {/* image start  */}
             <img
               className="rounded-xl w-full transition-transform duration-300 hover:scale-105 shadow-2xl"
-              src={
-                imageList.find((image) => image._id === post?.imageId)?.imageUrl
-              }
+              src=""
               alt="ui ux"
             />
             {/* image end  */}
@@ -74,6 +111,13 @@ const ArticleDetails = (): JSX.Element => {
             {/* end comments container  */}
           </article>
           <div>
+            {/* start suggested posts */}
+              <SuggestPosts
+                header={"Latest Articles"}
+                tags={tagsData}
+                className="mt-8 lg:mt-0 lg-max-w-xs"
+              />
+            {/* end suggested posts */}
             {/* start social share buttons */}
             <div className="mt-7 flex gap-4">
               <h2 className="font-Ubuntu font-medium text-dark-hard mb-4 md:text-xl">
