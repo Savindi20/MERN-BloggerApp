40e64500ca06f6f85ad22c0b172f078d364c6f60
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-20 13:37:37 +0530

    artical detail page updated

--- front-end/src/pages/ArticleDetails/SuggestPosts.tsx
+++ front-end/src/pages/ArticleDetails/SuggestPosts.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-function SuggestPosts() {
-  return (
-    <div>SuggestPosts</div>
-  )
+interface CardProps {
+  className?: string;
+  header: string;
+  tags: string[];
 }
 
-export default SuggestPosts
+const SuggestPosts = ({ className, header, tags }: CardProps) => {
+
+  return (
+    <div
+      className={`w-full shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] rounded-lg p-4 ${className}`}
+    >
+      <h2 className="font-Ubuntu font-medium text-dark-hard md:text-xl">
+        {header}
+      </h2>
+      <h2 className="font-Ubuntu font-medium text-dark-hard mt-8 md:text-xl">
+        Tags
+      </h2>
+    </div>
+  );
+};
+
+export default SuggestPosts;
