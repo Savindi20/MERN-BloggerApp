8d18d1a4b700bfa16e026dd4759974c93d2ed718
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-20 17:48:23 +0530

    Article detail start share button added

--- front-end/src/pages/ArticleDetails/ArticleDetails.tsx
+++ front-end/src/pages/ArticleDetails/ArticleDetails.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import MainLayout from "../../components/MainLayout/MainLayout";
+import SocialShareButtons from "../../components/SocialShareButtons/SocialShareButtons";
 
-function ArticleDetails() {
+const ArticleDetails = (): JSX.Element => {
   return (
-    <div>ArticleDetails</div>
-  )
-}
+    <div>
+      <MainLayout>
+        {/* start articleDetails section */}
+        <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
+          <div>
+            {/* start social share buttons */}
+            <div className="mt-7 flex gap-4">
+              <h2 className="font-Ubuntu font-medium text-dark-hard mb-4 md:text-xl">
+                Share On:
+              </h2>
+              <SocialShareButtons
+                url={encodeURI("")}
+                title={encodeURIComponent("")}
+              />
+            </div>
+            {/* end social share buttons */}
+          </div>
+        </section>
+        {/* end articleDetails section */}
+      </MainLayout>
+    </div>
+  );
+};
 
-export default ArticleDetails
+export default ArticleDetails;
