146d386e67c6ec08483ab1db358197264c623146
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-21 16:08:08 +0530

    post component updated with  property user and catergories

--- front-end/src/components/Post/Post.tsx
+++ front-end/src/components/Post/Post.tsx
@@ -1,7 +1,11 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { PostProps } from "../../types/PostProps";
+import { catergroy } from "../../types/Catergroy";
+import { UserProps } from "../../types/User";
 
 const Post: FC<PostProps> = (props) => {
+  const [categoryList, setCategoryList] = useState<catergroy[]>([]);
+  const [userList, setUserList] = useState<UserProps[]>([]);
 
   return (
     <>
@@ -9,9 +13,38 @@ const Post: FC<PostProps> = (props) => {
         <h3 className="text-center font-bold font-Ubuntu">{props.title}</h3>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
           <div className="flex flex-col gap-2 mt-2">
+            {props.date && (
+              <p>
+                <strong>Date</strong> : {props.date}
+              </p>
+            )}
+            {props.userName && (
+              <p>
+                <strong>User Name</strong> : 
+                {/* {props.userName} */}
+                {
+                  userList.find((user) => user._id === props.userName)?.name
+                }
+              </p>
+            )}
+            {props.categoryId && (
+              <p>
+                <strong>Category</strong> :
+                {
+                  categoryList.find(
+                    (category) => category._id === props.categoryId
+                  )?.categoryName
+                }
+              </p>
+            )}
+            {props.tags && (
+              <p>
+                <strong>Tags</strong> : {props.tags.join(", ")}
+              </p>
+            )}
           </div>
         </div>
-
+        
         <h2 className="text-center font-semibold font-babylonica">
           {props.caption}
         </h2>
