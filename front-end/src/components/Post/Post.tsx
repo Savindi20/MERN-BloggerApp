58afcbbac3ef0f4dcb0378b7d1c23891b89158b1
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-21 14:05:07 +0530

    post component updated with basic properties

--- front-end/src/components/Post/Post.tsx
+++ front-end/src/components/Post/Post.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { FC } from "react";
+import { PostProps } from "../../types/PostProps";
 
-function Post() {
+const Post: FC<PostProps> = (props) => {
+
   return (
-    <div>Post</div>
-  )
-}
+    <>
+      <div className="cursor-pointer p-6 bg-white text-slate-600 space-x-3 rounded-2xl shadow-2xl mt-1">
+        <h3 className="text-center font-bold font-Ubuntu">{props.title}</h3>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          <div className="flex flex-col gap-2 mt-2">
+          </div>
+        </div>
 
-export default Post
+        <h2 className="text-center font-semibold font-babylonica">
+          {props.caption}
+        </h2>
+        <p className="flex justify-center items-center font-opensans">
+          {props.description}
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Post;
