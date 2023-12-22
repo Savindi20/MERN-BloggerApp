f6ed64001d9fa30f449f0f00e091f7980bff8388
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-22 16:46:35 +0530

    user header section added with bbasic styles

--- front-end/src/components/Header/UserHeader/UserHeader.tsx
+++ front-end/src/components/Header/UserHeader/UserHeader.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function UserHeader() {
   return (
-    <div>UserHeader</div>
-  )
+    <section className="sticky top-0 left-0 right-0 z-50 bg-white">
+      <header
+        className="container mt-4 mx-auto mb-14 px-5 flex justify-center py-4 items-center"
+        style={{
+          boxShadow:
+            " rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset",
+          borderRadius: "15px",
+        }}
+      >
+        <div className="flex items-center">
+          {" "}
+          {/* Add flex and items-center classes */}
+          {/* <img className="w-16" src="" alt="logo" /> */}
+        </div>
+      </header>
+    </section>
+  );
 }
 
-export default UserHeader
+export default UserHeader;
