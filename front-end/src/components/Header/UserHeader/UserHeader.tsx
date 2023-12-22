c0ab14e58ec04ef5059441fcb2420ce47dc79f69
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-22 21:47:33 +0530

    NavItem function added

--- front-end/src/components/Header/UserHeader/UserHeader.tsx
+++ front-end/src/components/Header/UserHeader/UserHeader.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import { useNavigate } from "react-router-dom";
 
 interface NavItem {
@@ -45,6 +46,55 @@ interface HeaderProps {
   };
 }
 
+const NavItem: React.FC<HeaderProps> = ({ item }) => {
+  const [dropdown, setDropdown] = useState(false);
+
+  const toggleDropdownHandler = () => {
+    setDropdown((curState) => !curState);
+  };
+
+  return (
+    <li className="relative group">
+      {item.type === "link" ? (
+        <>
+          <a href="/" className="px-4 py-2">
+            {item.name}
+          </a>
+          <span className="cursor-pointer text-purple-600 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
+            /
+          </span>
+        </>
+      ) : (
+        <div className="flex flex-col items-center">
+          <button
+            className="px-4 py-2 flex gap-x-1 items-center"
+            onClick={toggleDropdownHandler}
+          >
+            <span>{item.name}</span>
+            <KeyboardArrowDownIcon />
+          </button>
+          <div
+            className={`${
+              dropdown ? "block" : "hidden"
+            } lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}
+          >
+            <ul className="bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden">
+              {item.items?.map((page, index) => (
+                <a
+                  key={index}
+                  href={page.link}
+                  className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
+                >
+                  {page.title}
+                </a>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
+    </li>
+  );
+};
 
 function UserHeader() {
   const [navIsVisible, setNavIsVisible] = useState(false);
@@ -81,6 +131,11 @@ function UserHeader() {
             navIsVisible ? "right-0" : "-right-full "
           } transition-all duration-300 mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static gap-x-9 items-center`}
         >
+          <ul className="text-white items-center gap-y-5 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold font-Ubuntu mx-auto">
+            {NavItemsInfo.map((item, index) => (
+              <NavItem key={index} item={item} />
+            ))}
+          </ul>
           <button
             onClick={() => navigate("/")}
             className="mt-5 lg:mt-0 font-Ubuntu border-2 border-purple-600 px-6 py-2 rounded-full text-purple-600 font-semibold hover:bg-purple-600 hover:text-white transition-all duration-300 text-center"
