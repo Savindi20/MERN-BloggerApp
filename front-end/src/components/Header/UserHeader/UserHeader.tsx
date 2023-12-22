b6309d5ff5677a2932c56a06b83f34bad860b3f7
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-22 17:39:13 +0530

    user header button and use states added

--- front-end/src/components/Header/UserHeader/UserHeader.tsx
+++ front-end/src/components/Header/UserHeader/UserHeader.tsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 function UserHeader() {
+  const navIsVisible = useState(false);
+  const navigate = useNavigate();
+
   return (
     <section className="sticky top-0 left-0 right-0 z-50 bg-white">
       <header
@@ -15,6 +20,18 @@ function UserHeader() {
           {" "}
           {/* Add flex and items-center classes */}
           {/* <img className="w-16" src="" alt="logo" /> */}
+        </div>
+        <div
+          className={`${
+            navIsVisible ? "right-0" : "-right-full "
+          } transition-all duration-300 mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static gap-x-9 items-center`}
+        >
+          <button
+            onClick={() => navigate("/")}
+            className="mt-5 lg:mt-0 font-Ubuntu border-2 border-purple-600 px-6 py-2 rounded-full text-purple-600 font-semibold hover:bg-purple-600 hover:text-white transition-all duration-300 text-center"
+          >
+            Log Out
+          </button>
         </div>
       </header>
     </section>
