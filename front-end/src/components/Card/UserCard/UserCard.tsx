99dc153c682ce22eba881893d57ffd372bb58c8d
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-24 14:16:22 +0530

    user card component updated with body

--- front-end/src/components/Card/UserCard/UserCard.tsx
+++ front-end/src/components/Card/UserCard/UserCard.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import images from "../../../constants/Images/images";
 import { PostProps } from "../../../types/PostProps";
 
 const Card: FC<PostProps> = (props) => { // props: PostProps
@@ -12,6 +13,24 @@ const Card: FC<PostProps> = (props) => { // props: PostProps
         className="w-full object-cover object-center h-auto md:h-52 lg:h-48 xl:h-60"
       />
       <div className="p-5">
+        <h2 className="font-Ubuntu font-bold text-xl text-dark-soft md:text-2xl lg:[28px]">
+          {props.title}
+        </h2>
+        <p className="text-dark-light mt-3 text-sm md:text-lg">
+          {props.caption}
+        </p>
+        <div className="flex justify-between flex-nowrap items-center mt-6">
+          <div className="flex items-center gap-x-2 md:gap-x-2.5">
+            <img
+              src={images.PostProfileImage}
+              alt="post profile"
+              className="w-9 h-9 md:w-10 md:h-10 "
+            />
+          </div>
+          <span className="font-bold text-dark-light italic text-sm md:text-base">
+            {props.date}
+          </span>
+        </div>
       </div>
     </div>
   );
