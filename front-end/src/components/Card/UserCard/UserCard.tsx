81deef1ee107184179061be004b743435bc387b0
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-24 13:37:52 +0530

    user card component updated

--- front-end/src/components/Card/UserCard/UserCard.tsx
+++ front-end/src/components/Card/UserCard/UserCard.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { FC } from "react";
+import { PostProps } from "../../../types/PostProps";
 
-function UserCard() {
+const Card: FC<PostProps> = (props) => { // props: PostProps
+
   return (
-    <div>UserCard</div>
-  )
-}
+    <div
+      className={`rounded-xl overflow-hidden shadow-2xl shadow-purple-500/20 transition-transform duration-300 hover:scale-105 ${ props.className }`}>
+      <img
+        src=""
+        alt="title"
+        className="w-full object-cover object-center h-auto md:h-52 lg:h-48 xl:h-60"
+      />
+      <div className="p-5">
+      </div>
+    </div>
+  );
+};
 
-export default UserCard
+export default Card;
