799faf1e49459b59566152fbe56b81f75e41aeca
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-31 00:59:22 +0530

    admin about page completed

--- front-end/src/pages/admin/AdminAbout/AdminAbout.tsx
+++ front-end/src/pages/admin/AdminAbout/AdminAbout.tsx
@@ -1,11 +1,73 @@
-import React from 'react'
+import React from "react";
+import AdminLayout from "../AdminLayout";
 
-const AdminAbout = () => {
+function AdminAbout() {
   return (
-    <div>
-      
-    </div>
-  )
+    <AdminLayout>
+      <section className="container mx-auto px-5 py-10">
+        <div className="container mx-auto px-4">
+          <div className="heading text-center">
+            {/* <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">
+              I'm Very Responsive to Messages
+            </h1> */}
+            <h1 className="text-2xl lg:text-3xl font-bold mb-4 text-gray-400">
+              About Us
+            </h1>
+          </div>
+          <div className="flex flex-col md:flex-row mt-8 gap-x-12 px-16">
+            <div className="w-full mb-8 md:mb-0">
+              <p className="font-opensans">
+                A blogger site about frameworks and UI/UX focuses on providing
+                valuable content related to software frameworks and user
+                interface/user experience design. It serves as a platform for
+                sharing knowledge, insights, tutorials, and best practices in
+                these areas to help developers and designers enhance their
+                skills and create exceptional digital experiences.
+                <br />
+                <br />
+                The about section of this blogger site would introduce the
+                purpose and goals of the site, emphasizing the importance of
+                frameworks and UI/UX in modern software development. It would
+                highlight the intention to empower readers with practical
+                information and resources to navigate the ever-evolving
+                landscape of frameworks and create user-centric designs.
+                <br />
+                <br />
+                The site would cover a wide range of topics related to
+                frameworks, including popular ones like React, Angular, Vue.js,
+                Django, Ruby on Rails, and more. It would provide in-depth
+                articles, tutorials, and guides on using these frameworks
+                effectively, showcasing real-world examples and best practices.
+                In terms of UI/UX, the blogger site would explore various
+                aspects of user interface and user experience design. It would
+                delve into topics such as usability principles, interaction
+                design, visual design, user research, prototyping, and usability
+                testing. The content would aim to help designers create
+                intuitive, aesthetically pleasing, and user-friendly interfaces
+                across different platforms and devices. The blogger behind this
+                site would have expertise and experience in frameworks and UI/UX
+                design. The about section would introduce the blogger's
+                background, qualifications, and passion for these areas,
+                establishing credibility and expertise. It would also highlight
+                the blogger's commitment to staying up-to-date with the latest
+                trends and advancements in frameworks and UI/UX, ensuring that
+                the content provided is relevant and valuable to the readers.
+                <br />
+                <br />
+                Overall, the blogger site about frameworks and UI/UX would
+                strive to be a go-to resource for developers and designers
+                seeking practical guidance, inspiration, and knowledge in these
+                domains. It would aim to foster a community of learners and
+                practitioners who are passionate about creating exceptional
+                digital experiences through the effective use of frameworks and
+                thoughtful UI/UX design.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </AdminLayout>
+  );
 }
 
-export default AdminAbout
+export default AdminAbout;
