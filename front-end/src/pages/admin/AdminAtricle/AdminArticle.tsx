d2976a088a4cae6005968ed3acf830bf06275627
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-05 01:33:21 +0530

    admin article page added and completed

---
+++ front-end/src/pages/admin/AdminAtricle/AdminArticle.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AdminLayout from "../AdminLayout";
+import Articles from "../../../components/Articles/Articles";
+
+const AdminArticle = () => {
+  return (
+    <>
+      <AdminLayout>
+        <Articles />
+      </AdminLayout>
+    </>
+  );
+};
+
+export default AdminArticle;
