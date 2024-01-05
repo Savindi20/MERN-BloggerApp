fc9c4095762c619ac45154194eb379a7c5480518
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-05 08:40:06 +0530

    admin article detail admin layout added

--- front-end/src/pages/admin/AdminArticleDetails/AdminArticleDetails.tsx
+++ front-end/src/pages/admin/AdminArticleDetails/AdminArticleDetails.tsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { PostDetails } from "../../../types/PostDetails";
 import { image } from "../../../types/Image";
 
 import axios from "../../../axios";
+import AdminLayout from "../AdminLayout";
 import images from "../../../constants/Images/images";
+import BreadCrumbs from "../../../components/BreadCrumbs/BreadCrumbs";
+import CommentsContainer from "../../../components/Comments/CommentsContainer/CommentsContainer";
+import SocialShareButtons from "../../../components/SocialShareButtons/SocialShareButtons";
 
 const breadCrumbsData = [
   { name: "Home", link: "/" },
@@ -105,6 +110,54 @@ const AdminArticleDetails = (): JSX.Element => {
 
   return (
     <div>
+      <AdminLayout>
+        {/* start articleDetails section */}
+        <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
+          <article className="flex-2">
+            <BreadCrumbs data={breadCrumbsData} />
+            {/* image start  */}
+            <img
+              className="rounded-xl w-full transition-transform duration-300 hover:scale-105 shadow-2xl"
+              src={
+                imageList.find((image) => image._id === post?.imageId)?.imageUrl
+              }
+              alt="ui ux"
+            />
+            {/* image end  */}
+            {/* start discription */}
+            <Link
+              to={""}
+              className="text-purple-600 text-sm font-Ubuntu inline-block mt-4 md:text-base"
+            >
+              {post?.title}
+            </Link>
+            <h1 className="text-xl font-medium font-Ubuntu mt-4 text-dark-hard md:text-[26px]">
+              {post?.caption}
+            </h1>
+            <div className="mt-4 text-dark-soft">
+              <p className="leading-7">{post?.description}</p>
+            </div>
+            {/* end discription */}
+            {/* start comments container  */}
+            <CommentsContainer className="mt-10" value="" logginedUserId="a" />
+            {/* end comments container  */}
+          </article>
+          <div>
+            {/* start social share buttons */}
+            <div className="mt-7 flex gap-4">
+              <h2 className="font-Ubuntu font-medium text-dark-hard mb-4 md:text-xl">
+                Share On:
+              </h2>
+              <SocialShareButtons
+                url={encodeURI("")}
+                title={encodeURIComponent("")}
+              />
+            </div>
+            {/* end social share buttons */}
+          </div>
+        </section>
+        {/* end articleDetails section */}
+      </AdminLayout>
     </div>
   );
 };
