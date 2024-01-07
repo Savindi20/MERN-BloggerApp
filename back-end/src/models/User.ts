d1d54e0f50436e53c1e2f7079d031d68163b0704
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-08 04:05:51 +0530

    User model details added

---
+++ back-end/src/models/User.ts
@@ -0,0 +1,27 @@
+import { Document, Schema, model } from "mongoose";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const UserSchema = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = model<IUser>("User", UserSchema);
