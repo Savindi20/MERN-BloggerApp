6441945c2aa72931ca2a68f8b6aa79eb49c3cc4b
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-08 04:48:44 +0530

    retrieveAllUsers method added

--- back-end/src/controllers/UserController.ts
+++ back-end/src/controllers/UserController.ts
@@ -1,4 +1,26 @@
-import express from "express";
+import { Request, RequestHandler, Response } from "express";
+import { User } from "../models/User";
 
 export default class UserController {
+
+  retrieveAllUsers: RequestHandler = async (
+    // retrieveAllUsers is the function to retrieve all the users
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    // Promise<Response> is the return type of the function
+    //read operation
+    try {
+      let users = await User.find(); // find() is used to find the documents
+
+      return res.status(200).json({ responseData: users });
+    } catch (error: unknown) {
+      // catch block is used to handle the errors
+      if (error instanceof Error) {
+        return res.status(500).json({ message: error.message });
+      } else {
+        return res.status(500).json({ message: "Unknown error occured." });
+      }
+    }
+  };
 }
--- back-end/src/routes/UserRoutes.ts
+++ back-end/src/routes/UserRoutes.ts
@@ -1,4 +1,21 @@
-import express from "express";
+import express, { Router } from "express";
+import UserController from "../controllers/UserController";
 
 export default class UserRoutes {
+  private router: Router = express.Router(); // Router is a class in express
+  private userController: UserController = new UserController(); // userController is the instance of the UserController class
+
+  constructor() {
+    // constructor() is used to initialize the class
+    this.configRoutes();
+  }
+
+  private configRoutes = (): void => {
+    // GET /api/v1/user
+    this.router.get("/", this.userController.retrieveAllUsers); // retrieveAllUsers is the function to retrieve all the users
+  };
+
+  public getRouter = (): Router => {
+    return this.router;
+  };
 }
