ae8ae2bdcf0024e8303a9bb04c68108ad355b49f
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-08 12:18:30 +0530

    index user rout added

--- back-end/src/routes/index.ts
+++ back-end/src/routes/index.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import UserRoutes from "./UserRoutes";
 
 const router: Router = Router(); // Router is a class in express
+const url_prefix = "/api/v1"; // url_prefix is the prefix of the url
+
+router.use(`${url_prefix}/user`, new UserRoutes().getRouter());
 
 export default router;
