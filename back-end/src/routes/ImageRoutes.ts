bb82811a70b59740f2fbe667c9f48d9cf33a6ecd
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-08 15:27:14 +0530

    image router, index router updated and completed

--- back-end/src/routes/ImageRoutes.ts
+++ back-end/src/routes/ImageRoutes.ts
@@ -1,4 +1,20 @@
-import express from "express";
+import express, { Router } from "express";
+import ImageController from "../controllers/ImageController";
 
-export default class ImageRoutes { 
+export default class ImageRoutes { // ImageRoutes is the class of the image routes
+  private router: Router = express.Router(); // Router is a class in express
+  private imageController: ImageController = new ImageController(); // imageController is the instance of the ImageController class
+
+  constructor() { // constructor() is used to initialize the class
+    this.configRoutes();
+  }
+
+  private configRoutes = (): void => { // configRoutes() is used to configure the routes
+    // GET /api/v1/image
+    this.router.get("/", this.imageController.retrieveAllImages);
+  };
+
+  public getRouter = (): Router => { 
+    return this.router; 
+  };
 }
--- back-end/src/routes/index.ts
+++ back-end/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import UserRoutes from "./UserRoutes";
+import ImageRoutes from "./ImageRoutes";
 
 const router: Router = Router(); // Router is a class in express
 const url_prefix = "/api/v1"; // url_prefix is the prefix of the url
 
 router.use(`${url_prefix}/user`, new UserRoutes().getRouter());
+router.use(`${url_prefix}/image`, new ImageRoutes().getRouter());
 
 export default router;
