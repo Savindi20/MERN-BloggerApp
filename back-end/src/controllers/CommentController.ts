97a30da616bf4d142903c891537031abc98d3346
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-10 18:37:30 +0530

    Comment controller rout post method completed

--- back-end/src/controllers/CommentController.ts
+++ back-end/src/controllers/CommentController.ts
@@ -1,5 +1,43 @@
-import express from "express";
+import { Request, RequestHandler, Response } from "express";
+import { Comment } from "../models/Comment";
 
 export default class CommentController {
-  
+
+  // CommentController is the controller of the comment
+  createComment: RequestHandler = async (
+    // createComment is the function to create a comment
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    // Promise<Response> is the return type of the function
+    try {
+      // destructuring assignment
+      const { comment } = req.body;
+
+      // check whether the relevant comment already exists or not
+      let createComment = await Comment.findOne({ comment: comment }); // findOne() is used to find the first document that matches the query
+      if (!createComment) {
+        // save comment only the comment  name is not existing
+        createComment = new Comment({ comment: comment }); // create a new comment
+        createComment = await createComment.save(); // save() is used to save the document
+
+        return res // return the response
+          .status(200)
+          .json({
+            message: "New comment added.!",
+            responseData: createComment,
+          });
+      } else {
+        return res.status(200).json({ message: "Already exists." });
+      }
+    } catch (error: unknown) {
+      // catch block is used to handle the errors
+      if (error instanceof Error) {
+        return res.status(500).json({ message: error.message });
+      } else {
+        return res.status(500).json({ message: "Unknown error occured." });
+      }
+    }
+  };
+
 }
--- back-end/src/routes/CommentRoutes.ts
+++ back-end/src/routes/CommentRoutes.ts
@@ -1,5 +1,20 @@
-import express from "express";
+import express, { Router } from "express";
+import CommentController from "../controllers/CommentController";
 
 export default class CommentRoutes { // CommentRoutes is the class of the comment routes
+  private router: Router = express.Router(); // Router is a class in express
+  private commentController: CommentController = new CommentController(); // commentController is the instance of the CommentController class
 
+  constructor() { // constructor() is used to initialize the class
+    this.configRoutes();
+  }
+
+  private configRoutes = (): void => { // configRoutes() is used to configure the routes
+    // POST /api/v1/comment
+    this.router.post("/", this.commentController.createComment);
+  };
+
+  public getRouter = (): Router => { 
+    return this.router; 
+  };
 }
