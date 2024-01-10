d36b4295ca11260a45093660bbc465010e572a31
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-10 17:49:12 +0530

    Comment model completed

--- back-end/src/models/Comment.ts
+++ back-end/src/models/Comment.ts
@@ -1,5 +1,18 @@
-import { Schema } from "mongoose";
+import { Schema, model, Document } from "mongoose";
+
+export interface IComment extends Document {
+  // IComment is the interface of the comment
+  comment: string;
+}
 
 const CommentSchema = new Schema( // CommentSchema is the schema of the comment
-
+  {
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
+
+export const Comment = model<IComment>("Comment", CommentSchema); // exporting the comment model
