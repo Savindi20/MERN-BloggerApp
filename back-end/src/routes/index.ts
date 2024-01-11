a307dbc07c46fcb3fa96e9bfbbd6ac769aebf489
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-11 18:28:11 +0530

    index file routs added and ux file added

--- back-end/src/routes/index.ts
+++ back-end/src/routes/index.ts
@@ -1,13 +1,17 @@
 import { Router } from "express";
 import UserRoutes from "./UserRoutes";
+import PostRoutes from "./PostRoutes";
 import CategoryRoutes from "./CategoryRoutes";
+import CommentRoutes from "./CommentRoutes";
 import ImageRoutes from "./ImageRoutes";
 
 const router: Router = Router(); // Router is a class in express
 const url_prefix = "/api/v1"; // url_prefix is the prefix of the url
 
 router.use(`${url_prefix}/user`, new UserRoutes().getRouter());
+router.use(`${url_prefix}/post`, new PostRoutes().getRouter());
 router.use(`${url_prefix}/category`, new CategoryRoutes().getRouter());
+router.use(`${url_prefix}/comment`, new CommentRoutes().getRouter());
 router.use(`${url_prefix}/image`, new ImageRoutes().getRouter());
 
 export default router;
